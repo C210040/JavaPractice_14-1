b7935ab2d8e1bb079f3915743e62ce89c07d0ae4
C210040 <92075597+C210040@users.noreply.github.com>
2023-01-05 17:30:44 +0900

    Add files via upload

---
+++ Account.java
@@ -0,0 +1,23 @@
+import javax.swing.event.AncestorEvent;
+
+public class Account {
+    String accountNumber;
+    int balance;
+    public String toString() {
+        return "￥" + this.balance + "(" + this.accountNumber + ")";
+    }
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o instanceof Account) {
+            Account a = (Account)o;
+            String an1 = this.accountNumber.trim();
+            String an2 = a.accountNumber.trim();
+            if (an1.equals(an2)) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
